6c6937a7c4b21830da089f4fb470b9507089cb15
TiboMessiaen <tibomessiaen@gmail.com>
2022-01-30 15:36:41 +0100

    GunSwitching

--- bestegame/Assets/Scripts/Gun.cs
+++ bestegame/Assets/Scripts/Gun.cs
@@ -5,16 +5,20 @@ public class Gun : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 5f;
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
-    //public GameObject impactEffect;
+    public GameObject impactEffect;
 
+    private float nextTimeToFire = 0f;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -33,7 +37,12 @@ public class Gun : MonoBehaviour
                 target.TakeDamage(damage);
             }
 
-            //Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 2f);
         }
     }
 }
---
+++ bestegame/Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if(selectedWeapon >= transform.childCount -1)
+            {
+                selectedWeapon = 0;
+            }
+            else
+            {
+                selectedWeapon++;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount-1;
+            }
+            else
+            {
+                selectedWeapon--;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            selectedWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2) && transform.childCount >=2)
+        {
+            selectedWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5) && transform.childCount >= 5)
+        {
+            selectedWeapon = 4;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6) && transform.childCount >= 6)
+        {
+            selectedWeapon = 5;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad7) && transform.childCount >= 7)
+        {
+            selectedWeapon = 6;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad8) && transform.childCount >= 8)
+        {
+            selectedWeapon = 7;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad9) && transform.childCount >= 9)
+        {
+            selectedWeapon = 8;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        // activating the selected weapon and disable others
+        int i = 0;
+        foreach(Transform weapon in transform)
+        {
+            if(i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
