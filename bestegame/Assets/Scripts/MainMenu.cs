2611b5b3988813e8f95e55dae71d657afa807c19
TiboMessiaen <tibomessiaen@gmail.com>
2022-01-31 18:29:45 +0100

    WorkingMenu

--- bestegame/Assets/Scripts/MainMenu.cs
+++ bestegame/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        // Cursor is locked if you go from map to menu
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
